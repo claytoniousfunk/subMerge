5fb02a83b59099ce7862c713696efc54c0eeb6a8
claytoniousfunk <cbennett0715@gmail.com>
2023-07-07 14:34:08 -0500

    adding main code

---
+++ subMerge.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Prompt user for directory path
+echo "Enter the path to the data directory:"
+read DATAPATH
+
+#find $DATAPATH -type f >> list-of-file-names.txt
+
+#echo "List of file names saved to list-of-file-names.txt"
+
+NUMBER_OF_FILES=$(ls "$DATAPATH"/*.root | wc -l)
+
+echo "Number of ROOT files in data directory = $NUMBER_OF_FILES"
+
+ITER=0
+
+for file in "$DATAPATH"/*.root; do
+
+    echo "Reading file $ITER of $NUMBER_OF_FILES"
+
+done
+
+
