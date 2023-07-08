6f5a0169428eb133c05b3c1d0a02cf465ef8d62e
claytoniousfunk <cbennett0715@gmail.com>
2023-07-08 16:10:11 -0500

    replate ++ with let command, remove cores option

--- subMerge.sh
+++ subMerge.sh
@@ -17,7 +17,7 @@ ITER=0
 
 for FILE in "$DATAPATH"/*.root; do
 
-    ((ITER++))
+    let ITER = ITER + 1
 
     echo "progress: $ITER / $NUMBER_OF_FILES"
     
@@ -26,12 +26,12 @@ for FILE in "$DATAPATH"/*.root; do
 	# hadd options:
 	# -k : skip corrupt or non-existant files, do not exit
 	# -v 0: set verbosity to 0
-	hadd -k -v 0 -j $NUMBER_OF_CORES $OUTPUT_FILENAME $FILE
+	hadd -k -v 0 $OUTPUT_FILENAME $FILE
 
     elif [ $ITER -gt 1 ] ; then
 
 	# add together new file merged file, save in TMP
-	hadd -k -v 0 -j $NUMBER_OF_CORES $TMP_FILENAME $FILE $OUTPUT_FILENAME
+	hadd -k -v 0 $TMP_FILENAME $FILE $OUTPUT_FILENAME
 
 	# remove the "old merged file"
 	rm $OUTPUT_FILENAME
