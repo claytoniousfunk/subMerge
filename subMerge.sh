bf126079e91698cc2268e7aa957eb0e788e0e7e1
claytoniousfunk <cbennett0715@gmail.com>
2023-07-08 16:05:14 -0500

    adding number of cores option

--- subMerge.sh
+++ subMerge.sh
@@ -11,6 +11,8 @@ echo "Number of ROOT files in data directory = $NUMBER_OF_FILES"
 OUTPUT_FILENAME="subMerge_output.root"
 TMP_FILENAME="subMerge_tmp.root"
 
+NUMBER_OF_CORES=8;
+
 ITER=0
 
 for FILE in "$DATAPATH"/*.root; do
@@ -24,12 +26,12 @@ for FILE in "$DATAPATH"/*.root; do
 	# hadd options:
 	# -k : skip corrupt or non-existant files, do not exit
 	# -v 0: set verbosity to 0
-	hadd -k -v 0 $OUTPUT_FILENAME $FILE
+	hadd -k -v 0 -j $NUMBER_OF_CORES $OUTPUT_FILENAME $FILE
 
     elif [ $ITER -gt 1 ] ; then
 
 	# add together new file merged file, save in TMP
-	hadd -k -v 0 $TMP_FILENAME $FILE $OUTPUT_FILENAME
+	hadd -k -v 0 -j $NUMBER_OF_CORES $TMP_FILENAME $FILE $OUTPUT_FILENAME
 
 	# remove the "old merged file"
 	rm $OUTPUT_FILENAME
