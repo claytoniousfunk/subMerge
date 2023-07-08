6f0a23d4eca8fe10536975ece1e38fa37d29fe65
claytoniousfunk <cbennett0715@gmail.com>
2023-07-08 14:10:00 -0500

    spacing

--- subMerge.sh
+++ subMerge.sh
@@ -29,7 +29,7 @@ for FILE in "$DATAPATH"/*.root; do
     elif($ITER -gt 1) ; then
 
 	# add together new file merged file, save in TMP
-	hadd  -k -v 0 $TMP_FILENAME $FILE $OUTPUT_FILENAME
+	hadd -k -v 0 $TMP_FILENAME $FILE $OUTPUT_FILENAME
 
 	# remove the "old merged file"
 	rm $OUTPUT_FILENAME
