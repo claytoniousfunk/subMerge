f6f8d20deef7dfe8f60ee3b99219de8747a7054d
claytoniousfunk <cbennett0715@gmail.com>
2023-07-10 09:03:47 -0500

    getting rid of verbosity options (causing errors on some machines?)

--- subMerge.sh
+++ subMerge.sh
@@ -25,13 +25,12 @@ for FILE in "$DATAPATH"/*.root; do
 
 	# hadd options:
 	# -k : skip corrupt or non-existant files, do not exit
-	# -v 0: set verbosity to 0
-	hadd -k -v 0 $OUTPUT_FILENAME $FILE
+	hadd -k $OUTPUT_FILENAME $FILE
 
     elif [ $ITER -gt 1 ] ; then
 
 	# add together new file merged file, save in TMP
-	hadd -k -v 0 $TMP_FILENAME $FILE $OUTPUT_FILENAME
+	hadd -k $TMP_FILENAME $FILE $OUTPUT_FILENAME
 
 	# remove the "old merged file"
 	rm $OUTPUT_FILENAME
